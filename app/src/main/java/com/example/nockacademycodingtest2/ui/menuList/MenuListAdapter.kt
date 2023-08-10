c92e9b35500f24b7290de35dc2cc129840bef392
ArmSiwatK <arm.siwatk@gmail.com>
2023-08-10 11:45:17 +0700

    feat: Total price calculation and display

--- app/src/main/java/com/example/nockacademycodingtest2/ui/menuList/MenuListAdapter.kt
+++ app/src/main/java/com/example/nockacademycodingtest2/ui/menuList/MenuListAdapter.kt
@@ -3,7 +3,9 @@ package com.example.nockacademycodingtest2.ui.menuList
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nockacademycodingtest2.R
@@ -18,6 +20,16 @@ class MenuListAdapter(private val menuItems: List<MenuItem>) :
         val menuName: TextView = itemView.findViewById(R.id.menuName)
         val menuPrice: TextView = itemView.findViewById(R.id.menuPrice)
         val checkIcon: ImageView = itemView.findViewById(R.id.checkIcon)
+    }
+
+    private fun calculateTotalPrice(): Double {
+        var total = 0.0
+        for (menuItem in menuItems) {
+            if (menuItem.isSelected) {
+                total += menuItem.price.toDouble()
+            }
+        }
+        return total
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
@@ -42,6 +54,18 @@ class MenuListAdapter(private val menuItems: List<MenuItem>) :
         holder.itemView.setOnClickListener {
             currentMenuItem.isSelected = !currentMenuItem.isSelected
             notifyDataSetChanged()
+
+            val totalLayout = holder.itemView.rootView.findViewById<LinearLayout>(R.id.totalLayout)
+            val totalPriceTextView = holder.itemView.rootView.findViewById<TextView>(R.id.totalPriceTextView)
+            val nextButton = holder.itemView.rootView.findViewById<Button>(R.id.nextButton)
+
+            val total = calculateTotalPrice()
+            if (total > 0) {
+                totalLayout.visibility = View.VISIBLE
+                totalPriceTextView.text = "Total Price: $total THB"
+            } else {
+                totalLayout.visibility = View.GONE
+            }
         }
     }
 
